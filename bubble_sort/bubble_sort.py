a56bfde81184d108abb03a20d5fcbe207b844dbf
Alvin Kyalo <kyaloalviin@gmail.com>
2025-06-20 13:40:52 +0300

    implement bubble sort algorithm

--- bubble_sort/bubble_sort.py
+++ bubble_sort/bubble_sort.py
@@ -1,3 +1,18 @@
+import numbers
+from webbrowser import get
+
+
+
+# TODO: Implement bubble sort
 def bubble_sort(unsorted_list):
-    # TODO: Implement bubble sort
-    pass
+    n = len(unsorted_list)
+    for i in range(n):
+        for j in range(0, n-i-1):
+            if unsorted_list[j] > unsorted_list[j+1]:
+                unsorted_list[j], unsorted_list[j+1] = unsorted_list[j+1], unsorted_list[j]
+
+numbers = [74, 35, 36, 25, 32, 24, 11, 0]
+print("Original list:", numbers)
+bubble_sort(numbers)
+print("Sorted numbers:", numbers)
+pass
