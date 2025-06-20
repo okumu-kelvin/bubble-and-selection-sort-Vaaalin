b653488aeb66ff66095c75a85a41604775757725
Alvin Kyalo <kyaloalviin@gmail.com>
2025-06-20 13:53:59 +0300

    implement selection sort algorithm

--- selection_sort/selection_sort.py
+++ selection_sort/selection_sort.py
@@ -1,3 +1,15 @@
 def selection_sort(arr):
+    n = len(arr)
+    for i in range(n):
+        min_index = i
+        for j in range(i+1, n):
+            if arr[j] < arr[min_index]:
+                min_index = j
+        arr[i], arr[min_index] = arr[min_index], arr[i]
+numbers = [74, 35, 36, 25, 32, 24, 11, 0]
+print("Original list:", numbers)
+selection_sort(numbers)
+print("Sorted numbers:", numbers)
+
     # TODO: Implement selection sort
-    pass
+pass
